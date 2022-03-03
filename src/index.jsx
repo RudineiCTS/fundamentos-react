637e3535bcd44b8c697a0abd284c13ba7bf2f672
Rudinei Augusto Costa Ferreira <rudinei.ferreira@linx.com.br>
2022-03-03 11:40:22 -0300

    config webpack

--- src/index.jsx
+++ src/index.jsx
@@ -2,4 +2,4 @@ import {render} from 'react-dom';
 import {App} from './App';
 
 //render recebe dois parametros, o que eu quero renderizar e dentro de qual elemento ele vai renderizar
-render(<h1>teste</h1>, document.getElementById('root'));
+render(<App/>, document.getElementById('root'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path')
-
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports={
     entry: path.resolve(__dirname, 'src', 'index.jsx'),
@@ -7,9 +7,20 @@ module.exports={
         path: path.resolve(__dirname, 'dist'),
         filename:'bundle.js'
     },
+    mode: 'development',
     resolve:{
         extensions:['.js', '.jsx']
     },
+    //webpack vai observar as alterações do código, para que cada alteração, seja criado o bundle
+    //essa config precisa receber o local que fica o arquivo html estatico 
+    devServer:{
+        static: path.resolve(__dirname, 'public'),
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'public', 'index.html'),
+        })
+    ],
     module: {
         rules:[
             {
