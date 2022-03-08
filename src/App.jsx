9902b725eeabf1c84070482fdf102a6391463b35
Rudinei Costa <costaaugusto177@outlook.com>
2022-03-07 23:30:05 -0300

    create repositorylist component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
+import { RepositoryList } from './components/RepositoryList';
 import './styles/global.scss';
 
 export function App (){
     return (
-        <h1>hellow woels</h1>
+        <RepositoryList/>
     )
 }
---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,14 @@
+export function RepositoryList(){
+  return(
+    <section>
+      <ul>
+        <li>
+          <strong>unform</strong>
+          <p>description to repository</p>
+
+          <a href="#"></a>
+        </li>
+      </ul>
+    </section>
+  )
+}
